4edb835e57d7bf67752b2414312da8895a4cfc67
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-05-12 10:32:38 -0400

    Se añadio el selector de tipos en el panel de administración

--- pokedex/models.py
+++ pokedex/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=50)
-    height = models.FloatField()
-    weight = models.FloatField()
+    POKEMON_TYPES = {
+        ('A', 'Agua'),
+        ('F', 'Fuego'),
+        ('T', 'Tierra'),
+        ('P', 'Planta'),
+        ('E', 'Eléctrico'),
+        ('L', 'Lagartija')
+    }
+    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null = False)
+    height = models.FloatField(decimal_places=4, max_digits=6)
+    weight = models.FloatField(decimal_places=4, max_digits=6)
 
     def __str__(self):
         return self.name

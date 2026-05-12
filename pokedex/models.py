eec4b69d9c19ff3315c2dd79b10ffb452cdbe054
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-05-12 14:43:21 -0400

    Seccion trainers añadido

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    birth_date = models.DateField()
+    level = models.IntegerField(default=1)
+    city = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Pokemon(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30, null=False)
     POKEMON_TYPES = {
         ('A', 'Agua'),
         ('F', 'Fuego'),
@@ -10,9 +20,9 @@ class Pokemon(models.Model):
         ('E', 'Eléctrico'),
         ('L', 'Lagartija')
     }
-    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null = False)
-    height = models.FloatField(decimal_places=4, max_digits=6)
-    weight = models.FloatField(decimal_places=4, max_digits=6)
+    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 def index(request):
     pokemons = Pokemon.objects.all()
+    trainers = Trainer.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({'pokemons': pokemons, 'trainers': trainers}, request))
+
+def trainer_detail(request, id):
+    trainer = Trainer.objects.get(id=id)
+    template = loader.get_template('display_trainer.html') # Necesitarás crear este HTML
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
 
 def pokemon(request, id: int):
     pokemon = Pokemon.objects.get(id=id)

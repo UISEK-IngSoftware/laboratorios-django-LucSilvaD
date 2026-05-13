6858e83d8e0d9a50f528846e46bcdacf317196da
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-05-12 21:44:34 -0400

    Llaves foraneas de trainers - y modificacion de trainers en el panel de admin

--- pokedex/models.py
+++ pokedex/models.py
@@ -23,6 +23,7 @@ class Pokemon(models.Model):
     type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
     height = models.DecimalField(max_digits=5, decimal_places=2)
     weight = models.DecimalField(max_digits=6, decimal_places=2)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name

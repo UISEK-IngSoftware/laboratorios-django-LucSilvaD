d788450da481ee108c9b76ae05760d9b4ef1a266
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-05-16 13:20:17 -0400

    Laboratorio 5 - Parte 2 Completado

---
+++ pokedex/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Pokemon
+
+class PokemonForm(forms.ModelForm):
+    class Meta:
+        model = Pokemon
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs.update({'class': 'form-select'})
+            elif isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs.update({'class': 'form-check-input'})
+            else:
+                field.widget.attrs.update({'class': 'form-control'})
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,25 +1,42 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, redirect
 from .models import Pokemon, Trainer
+from .forms import PokemonForm
 
 def index(request):
     pokemons = Pokemon.objects.all()
     trainers = Trainer.objects.all()
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons, 'trainers': trainers}, request))
+    return render(request, "index.html", {"pokemons": pokemons, "trainers": trainers})
 
-def trainer_detail(request, id):
-    trainer = Trainer.objects.get(id=id)
-    template = loader.get_template('display_trainer.html') # Necesitarás crear este HTML
-    context = {
-        'trainer': trainer
-    }
-    return HttpResponse(template.render(context, request))
-
-def pokemon(request, id: int):
+def pokemon(request, id):
     pokemon = Pokemon.objects.get(id=id)
-    template = loader.get_template('display_pokemon.html')
-    context = {
-        'pokemon': pokemon
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, "display_pokemon.html", {"pokemon": pokemon})
+
+def trainer(request, id):
+    trainer = Trainer.objects.get(id=id)
+    return render(request, "display_trainer.html", {"trainer": trainer})
+
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def edit_pokemon(request, id):
+    pokemon = Pokemon.objects.get(id=id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def delete_pokemon(request, id):
+    pokemon = Pokemon.objects.get(id=id)
+    pokemon.delete()
+    return redirect('pokedex:index')

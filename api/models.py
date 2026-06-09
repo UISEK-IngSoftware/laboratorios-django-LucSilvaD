a6337f5ee96420880e39563ac5b3bfa012ca2126
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-06-09 08:24:17 -0400

    creacion de carpeta api con archivos correspondientes + serializers.py

---
+++ api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    birth_date = models.DateField()
+    level = models.IntegerField(default=1)
+    city = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    POKEMON_TYPES = {
+        ('A', 'Agua'),
+        ('F', 'Fuego'),
+        ('T', 'Tierra'),
+        ('P', 'Planta'),
+        ('E', 'Eléctrico'),
+        ('L', 'Lagartija')
+    }
+    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True)
+    picture = models.ImageField(upload_to='pokemon_images', null=True, blank=True)
+
+    def __str__(self):
+        return self.name

9c0e22a09f0febb962717db685b8f3805b26b1af
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-07-11 21:23:09 -0400

    Laboratorio 10 terminado

--- lab8/settings.py
+++ lab8/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'pokedex',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -161,3 +163,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
